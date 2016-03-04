889627b9fa148d0c0fece01c09880a10b7df40a8
Oleg Kotenko <oko10ko@ukr.net>
2016-03-04 17:10:14 +0200

    clear code & added auto-mount initializer

--- app/controllers/rel_gram/application_controller.rb
+++ app/controllers/rel_gram/application_controller.rb
@@ -1,7 +1,4 @@
 module RelGram
   class ApplicationController < ActionController::Base
-    # def index
-    #   @structure = RelGram::Structure.new.get_structure
-    # end
   end
 end
--- lib/rel_gram/engine.rb
+++ lib/rel_gram/engine.rb
@@ -1,5 +1,11 @@
 module RelGram
   class Engine < ::Rails::Engine
     isolate_namespace RelGram
+
+    initializer 'rel_gram', before: :load_config_initializers do |app|
+      Rails.application.routes.prepend do
+        mount RelGram::Engine, at: '/rel_gram'
+      end
+    end
   end
 end
