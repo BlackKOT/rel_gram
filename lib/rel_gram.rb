2fded7414ab92c3a711ef95ecf7ba1ed6b8c65e0
Oleg Kotenko <oko10ko@ukr.net>
2016-03-10 18:37:21 +0200

    unifying get_foreign_key method

--- lib/rel_gram.rb
+++ lib/rel_gram.rb
@@ -6,40 +6,41 @@ module RelGram
     def get_structure
       Rails.application.eager_load! unless Rails.configuration.cache_classes
       output_data = ActiveRecord::Base.descendants.each_with_object({tables: {}, rels: {}}) do |model, models_data|
-        begin
-          columns = model.columns.each_with_object({}) do |column, columns_data|
-            columns_data[column.name] = {type: column.sql_type, null: column.null, default: column.default}
-          end
-          models_data[:tables][model.name] = {
+        # begin
+        columns = model.columns.each_with_object({}) do |column, columns_data|
+          columns_data[column.name] = {type: column.sql_type, null: column.null, default: column.default}
+        end
+        models_data[:tables][model.name] = {
             attributes: columns,
             external: true,
             parent: model.parent.name
-          }
+        }
 
-          reflections = model.reflections.each_with_object({}) do |(reflection_name, reflection_object), reflections_data|
-            dest_model_name = (reflection_object.options[:class_name] || reflection_name.to_s).singularize.camelize
-            through_model = reflection_object.options[:through]
-            if through_model.present?
-              reflections_data[through_model.singularize] = {
+        reflections = model.reflections.each_with_object({}) do |(reflection_name, reflection_object), reflections_data|
+          dest_model_name = (reflection_object.options[:class_name] || reflection_name.to_s).singularize.camelize
+          through_model = reflection_object.options[:through]
+          if through_model.present?
+            reflections_data[through_model.to_s.singularize.camelize] = {
                 foreign_key: "#{model.name.underscore}_id",
                 rel_type: :through
-              }
-              models_data[:rels][dest_model_name][through_model.singularize] = {
+            }
+            models_data[:rels][dest_model_name] ||= {}
+            models_data[:rels][dest_model_name][through_model.to_s.singularize.camelize] = {
                 foreign_key: "#{dest_model_name.underscore}_id",
                 rel_type: :through
-              }
-            end
-            reflections_data[dest_model_name] = {
-              foreign_key: get_foreign_key(reflection_name, reflection_object, model),
+            }
+          end
+          reflections_data[dest_model_name] = {
+              foreign_key: get_foreign_key(reflection_name, model),
               rel_type: reflection_object.macro,
               alias: reflection_object.options[:class_name].present? ? reflection_name : nil,
               options: reflection_object.options
-            }
-          end
-          models_data[:rels][model.name] = reflections
-        rescue => e
-          p "Error! #{model} excluded from diagram. #{e.message}"
+          }
         end
+        models_data[:rels][model.name] = reflections
+        # rescue => e
+        #   p "Error! #{model} excluded from diagram. #{e.message}"
+        # end
       end
 
       Dir["#{Rails.root}/app/models/**/*.rb"].map do |f|
@@ -55,9 +56,8 @@ module RelGram
     end
 
     private
-    def get_foreign_key(r_name, r_object, model)
-      r_name.respond_to?(:foreign_key) ?
-        r_name.foreign_key : r_object.options[:foreign_key] || 'id'
+    def get_foreign_key(r_name, model)
+      model.reflect_on_association(r_name.to_sym).foreign_key
     end
   end
 end
